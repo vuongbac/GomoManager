2f1bc70370c0e94b529abcec4f9f14b1984f164d
Bac <ngonhatdongho>
2021-01-15 18:10:03 +0700

    áda

--- GomoManager/Admin/Employees/EmployeesViewController.swift
+++ GomoManager/Admin/Employees/EmployeesViewController.swift
@@ -5,14 +5,14 @@ import Firebase
 
 class EmployeesViewController: UIViewController {
     
-    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var tableView: UITableView!
     var employees = [Employees]()
     let idAdmin = Defined.defaults.value(forKey: "idAdmin") as? String
     
     override func viewDidLoad() {
         super.viewDidLoad()
         getDataEmployees()
-        EmployeesCell1.registerCellByNib(collectionView)
+        EmployeesCell.registerCellByNib(tableView)
     }
     
     func getDataEmployees(){
@@ -34,7 +34,7 @@ class EmployeesViewController: UIViewController {
                         print(em)
                     }
                 }
-                self.collectionView.reloadData()
+                self.tableView.reloadData()
             }
         }
     }
@@ -46,30 +46,64 @@ class EmployeesViewController: UIViewController {
     }
     
 }
-extension EmployeesViewController: UICollectionViewDelegate, UICollectionViewDataSource{
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+
+extension EmployeesViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return employees.count
     }
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = EmployeesCell1.loadCell(collectionView, path: indexPath) as! EmployeesCell1
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = EmployeesCell.loadCell(tableView) as! EmployeesCell
         cell.setUp(data: employees[indexPath.row])
         return cell
     }
     
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: Constans.detailEmployes) as! DetailEmployessViewController
-        let e = employees[indexPath.row]
-        vc.nameF = e.name ?? ""
-        vc.emailE = e.email ?? ""
-        vc.addF = e.address ?? ""
-        vc.phoneF = e.phone ?? ""
-        vc.imageE = e.avatar ?? ""
-        vc.ageF = e.birtday ?? ""
-        vc.sexF = e.gender ?? ""
-        self.navigationController?.pushViewController(vc, animated: true)
+    func tableView(_ tableView: UITableView, contextMenuConfigurationForRowAt indexPath: IndexPath, point: CGPoint) -> UIContextMenuConfiguration? {
+        return UIContextMenuConfiguration(identifier: nil, previewProvider: nil, actionProvider: {_ in
+            return self.addMenuItems(for: indexPath.row)
+        })
     }
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 140
+    }
     
+ 
+    
+    func addMenuItems(for index:Int) -> UIMenu {
+        let e = employees[index]
+        let menuItems = UIMenu(title: "", options: .displayInline, children: [
+            UIAction(title: "Xóa", image: UIImage(named: "ic_delete"), handler: { (_) in
+                let strEmail = e.email
+                print("say hi\(e.email)")
+                let tempString = strEmail!.split(separator: "@")
+                print("say hu\(tempString)")
+                let em = tempString[0]
+                let user = Auth.auth().currentUser
+                user?.delete { error in
+                    if let error = error {
+                        print(error)
+                    } else {
+                        Defined.ref.child("Account").child(self.idAdmin ?? "").child("Employees").child("\(em)").removeValue { (error, reference) in}
+                        self.navigationController?.popViewController(animated: true)
+                        
+                    }
+                }
+               
+            }),
+            
+            UIAction(title: "Sửa", image: UIImage(named: "ic_task_list"), handler: { (_) in
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: Constans.detailEmployes) as! DetailEmployessViewController
+                vc.nameF = e.name ?? ""
+                vc.emailE = e.email ?? ""
+                vc.addF = e.address ?? ""
+                vc.phoneF = e.phone ?? ""
+                vc.imageE = e.avatar ?? ""
+                vc.ageF = e.birtday ?? ""
+                vc.sexF = e.gender ?? ""
+                self.present(vc, animated: true, completion: nil)
+            }),
+        ])
+        return menuItems
+    }
 }
-
--- GomoManager/Admin/Views/EmployeesCell.swift
+++ GomoManager/Admin/Views/EmployeesCell.swift
@@ -4,17 +4,38 @@ import UIKit
 
 class EmployeesCell: BaseTBCell {
 
-    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var subView1: UIView!
+    @IBOutlet weak var subView2: UIView!
+    @IBOutlet weak var avatar: UIImageView!
     @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var lbGerder: UILabel!
+    @IBOutlet weak var lbEmail: UILabel!
+    @IBOutlet weak var lbBrithday: UILabel!
+    @IBOutlet weak var lbAdd: UILabel!
+    @IBOutlet weak var lbPhone: UILabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        subView1.addBoder(radius: 20, color: #colorLiteral(red: 0.2274329066, green: 0.5870787501, blue: 0.9447389245, alpha: 1))
+        subView2.addBoder(radius: 20, color: #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1))
+        subView1.addShadow(radius: 20)
+        avatar.addShadow(radius: 20)
+        avatar.addBoder(radius: avatar.bounds.size.height/2, color: #colorLiteral(red: 0.2274329066, green: 0.5870787501, blue: 0.9447389245, alpha: 1))
     }
+    
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
     }
     
-
+    func setUp(data: Employees) {
+        lbAdd.text = data.address
+        lbEmail.text = data.email
+        lbPhone.text = data.phone
+        lbGerder.text = "Giới tính: \(data.gender)"
+        lbBrithday.text = data.birtday
+        userName.text = data.name
+        avatar.sd_setImage(with: URL(string: data.avatar ?? ""), completed: nil)
+    }
     
 }
